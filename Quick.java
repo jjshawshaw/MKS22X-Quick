fed13c93e7e4a8ee08ef2b8293d8b6d669198812
jjshawshaw <jshaw00@stuy.edu>
2019-03-13 11:36:26 -0400

    wrote quicksort method

--- Quick.java
+++ Quick.java
@@ -5,6 +5,18 @@ public class Quick{
    return 0;
  }
 
+ /*Modify the array to be in increasing order.
+  */
+  public static void quicksort(int[] data){
+    quicksort(data, 0, data.length - 1);
+  }
+  public static void quicksort(int[] data, int lo, int hi){
+    if (lo >= hi) return;
+    int pivot = partition(data, lo, hi);
+    quicksort(data, lo, pivot - 1);
+    quicksort(data, pivot + 1, hi);
+  }
+
 
 /*Modify the array such that:
  *1. Only the indices from start to end inclusive are considered in range
@@ -18,26 +30,26 @@ public class Quick{
 private static int partition ( int [] data, int start, int end){
    int pivot = (int)(Math.random() * (end - start) + start);
    int n = data[pivot];
-  System.out.println(n);
+  System.out.println("n: " + n);
    while (start < end){
-     if (start == pivot && end != start){
+     if (start >= pivot && end != start){
        int temp = data[pivot + 1];
        data[pivot + 1] = data[pivot];
        data[pivot] = temp;
        pivot++;
        if (temp < data[pivot]) start++;
      }
-     if (end == pivot && end != start){
+     if (end <= pivot && end != start){
        int temp = data[pivot - 1];
        data[pivot - 1] = data[pivot];
        data[pivot] = temp;
        pivot--;
        if (temp > data[pivot]) end--;
      }
-     if (data[start] < data[pivot]){
+     if (data[start] <= data[pivot] && end != start){
        start++;
      }
-     if (data[start] > data[pivot]){
+     if (data[start] > data[pivot] && end != start){
        if (pivot == end) pivot = start;
        int temp = data[end];
        data[end] = data[start];
@@ -49,8 +61,12 @@ private static int partition ( int [] data, int start, int end){
  }
 
  public static void main(String[] args){
-   int[] a = new int[]{998,997,996,4,1,0,3,2,995,994,999};
-   System.out.println(partition(a, 0, 10));
+   int[] a = new int[]{999,999,999,4,1,0,3,2,999,999,999};
+   System.out.println("pivot: " + partition(a, 0, 10));
+   for (int i : a){
+     System.out.print(i + " ");
+   }
+   quicksort(a);
    for (int i : a){
      System.out.print(i + " ");
    }
